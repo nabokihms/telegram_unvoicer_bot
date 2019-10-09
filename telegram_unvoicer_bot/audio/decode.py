0bf91c91fdafb7a105d21d1555e7da9655a6f5b9
Maksim Nabokikh <32434187+nabokihms@users.noreply.github.com>
2019-10-10 00:34:53 +0400

    BOT-10:
    * Update description

--- telegram_unvoicer_bot/audio/decode.py
+++ telegram_unvoicer_bot/audio/decode.py
@@ -14,7 +14,7 @@ __all__ = [
 _DECODE_MAX_LENGTH: int = 90
 
 
-# todo: избавиться от использования библиотеки
+# todo: do not use library here
 def _google_speech_recognise(file_path: str) -> str:
     r = sr.Recognizer()
     with sr.AudioFile(file_path) as source:
@@ -28,7 +28,7 @@ async def decode_audio(file_path: str) -> str:
     new_file_path = f'{path}.wav'
     await AUDIO_FILES_CONVERTERS[ext](file_path, new_file_path)
 
-    # todo: декодировать длинные аудиофайлы чанками
+    # todo: decode data by chunks
     seconds = float(
         subprocess.check_output([
             'soxi', '-D', new_file_path
@@ -36,8 +36,8 @@ async def decode_audio(file_path: str) -> str:
     )
 
     if seconds > _DECODE_MAX_LENGTH:
-        return f'Длина файла составляет {seconds} секунды! ' \
-               f'Я не умею декодировать настолько большие \U0001f605'
+        return f'Audio file duration is {seconds} seconds! ' \
+               f'I can\'t decode it \U0001f605'
 
     text = _google_speech_recognise(new_file_path)
 
